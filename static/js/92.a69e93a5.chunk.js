e2d868ca02379901a7d359db00e3ef7b4c3584b4
AntOn2415 <115253570+AntOn2415@users.noreply.github.com>
2023-10-19 07:05:50 +0000

    Deploying to gh-pages from @ AntOn2415/car-rental@3ad0027a6d4d8c9a648f467f1674e9e40e349ea7 🚀

---
+++ static/js/92.a69e93a5.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcar_rental=self.webpackChunkcar_rental||[]).push([[92],{92:function(n,e,t){t.r(e),t.d(e,{default:function(){return f}});var r,i=t(168),c=t(867).ZP.div(r||(r=(0,i.Z)(["\n  display: flex;\n  flex-direction: column;\n  justify-content: center;\n  align-items: center;\n  gap: 30px;\n  margin: auto;\n  width: 100%;\n  height: 100vh;\n"]))),u=t(184),a=function(){return(0,u.jsx)(c,{children:"favorites"})};function f(){return(0,u.jsx)(a,{})}}}]);
+//# sourceMappingURL=92.a69e93a5.chunk.js.map
